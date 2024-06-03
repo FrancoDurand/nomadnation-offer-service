5571f7fd35cfdde773fd00403ef48003a7482c49
Franco Durand <francoduve.25@gmail.com>
2024-06-02 23:09:46 -0500

    add images

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express"
 import cors from "cors"
 import offerRouter from "./routes/offer-router";
+import path from "path"
 
 const app = express();
 const port = 3001;
 
+const imagesDir = path.join(__dirname, "../images");
+
 app.use(cors());
 app.use(express.json());
+app.use("/images", express.static(imagesDir));
 
 app.use(offerRouter);
 
